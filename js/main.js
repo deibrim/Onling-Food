290e8602c07eefbbbc7307e261ffb17972f971cf
0zzycodes <horlalekan123@gmail.com>
2019-12-28 11:45:40 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+(function ($) {
+
+	"use strict";
+	var fullHeight = function () {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function () {
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('nav .dropdown').hover(function () {
+		var $this = $(this);
+		// 	 timer;
+		// clearTimeout(timer);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function () {
+		var $this = $(this);
+		$this.removeClass('show');
+		$this.find('> a').attr('aria-expanded', false);
+		$this.find('.dropdown-menu').removeClass('show');
+	});
+
+
+	$('#dropdown04').on('show.bs.dropdown', function () {
+		console.log('show');
+	});
+
+	// scroll
+	var scrollWindow = function () {
+		$(window).scroll(function () {
+			var $w = $(this),
+				st = $w.scrollTop(),
+				navbar = $('.ftco_navbar'),
+				sd = $('.js-scroll-wrap');
+
+			if (st > 150) {
+				if (!navbar.hasClass('scrolled')) {
+					navbar.addClass('scrolled');
+				}
+			}
+			if (st < 150) {
+				if (navbar.hasClass('scrolled')) {
+					navbar.removeClass('scrolled sleep');
+				}
+			}
+			if (st > 350) {
+				if (!navbar.hasClass('awake')) {
+					navbar.addClass('awake');
+				}
+
+				if (sd.length > 0) {
+					sd.addClass('sleep');
+				}
+			}
+			if (st < 350) {
+				if (navbar.hasClass('awake')) {
+					navbar.removeClass('awake');
+					navbar.addClass('sleep');
+				}
+				if (sd.length > 0) {
+					sd.removeClass('sleep');
+				}
+			}
+		});
+	};
+	scrollWindow();
+
+
+
+	$('#book_date').datepicker({
+		'format': 'm/d/yyyy',
+		'autoclose': true
+	});
+	$('#book_time').timepicker();
+
+})(jQuery);
